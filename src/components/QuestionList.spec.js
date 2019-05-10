86e9822fae9f885bbff458d77d2b65a260c81757
Luke Parker <moongateuk@gmail.com>
2019-05-10 11:36:36 +0100

    Made test tests.

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,16 @@
 describe('The Question List', () => {
-    it ("shoud idsplay a list of items", () => {
-        
+    beforeEach(() => {
+        console.log("Before each!");
+    })
+
+    beforeAll(()=>{
+        console.log("Before all!");
+    })
+    it ("shoud display a list of items", () => {
+        expect (40 + 2).toEqual(42);
     });
+
+    it.skip ("should break", () => {
+        expect (45 + 5).toEqual(40);
+    })
 });
